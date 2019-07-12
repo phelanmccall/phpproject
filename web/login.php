b46efc54db87db169a61343d5716a9c1a92fe5a8
Phelan <phelanmccall@yahoo.com>
2019-07-12 16:17:34 -0700

    redirect

--- web/login.php
+++ web/login.php
@@ -1,8 +1,6 @@
-   <?php include_once("navbar.php"); ?>
-   
-   <h1><?php
+<?php
         session_start();
         $_SESSION["username"] = $_POST["name"];
         echo $_SESSION["username"];
-
-    ?></h1>
+        header("Location: {$_SERVER["HTTP_REFERER"]}");
+    ?>
