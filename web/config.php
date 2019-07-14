e1394c3a1c2af4e4218765afd9208ec5bcd03d84
Phelan <phelanmccall@yahoo.com>
2019-07-14 11:26:18 -0700

    testing cleardb

--- web/config.php
+++ web/config.php
@@ -1,6 +1,11 @@
 <?php 
     session_start();
-    $conn = mysqli_connect("localhost", "root", "", "complete-blog-php");
+    $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server   = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db       = substr($cleardb_url["path"],1);
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
     if (!$conn) {
 		die("Error connecting to database: " . mysqli_connect_error());
 	}
