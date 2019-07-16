7c195bc72442423e5b41bc521cfea16fb8e68ae1
Phelan <phelanmccall@yahoo.com>
2019-07-16 00:36:47 -0700

    update config

--- web/config.php
+++ web/config.php
@@ -7,6 +7,18 @@
     $cleardb_db       = substr($cleardb_url["path"],1);
     $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
     if (!$conn) {
-		die("Error connecting to database: " . mysqli_connect_error());
-	}
+		  die("Error connecting to database: " . mysqli_connect_error());
+    }
+    
+    $sql = "CREATE TABLE `data` (
+      `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      `title` varchar(255) NOT NULL
+      )";
+    if(mysqli_query($conn, $sql)){
+      echo 'Table created.';
+    }else{
+      echo "ERROR: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
 ?>
