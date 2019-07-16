2172d318a6d53c9959fa906e62eff3db05c98bd9
Phelan <phelanmccall@yahoo.com>
2019-07-16 01:28:35 -0700

    yup

--- web/config.php
+++ web/config.php
@@ -10,22 +10,7 @@
 		  die("Error connecting to database: " . mysqli_connect_error());
     }
     
-    $sql = "DROP TABLE IF EXISTS `data`";
-    $sql2 = "CREATE TABLE `data` (
-        `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        `name` varchar(255) NOT NULL
-    )";
-    if(mysqli_query($conn, $sql)){
-      echo 'Table dropped.';
-    }else{
-      echo "ERROR: " . mysqli_error($conn);
-    }
-    if(mysqli_query($conn, $sql2)){
-      echo 'Table creted.';
-    }else{
-      echo "ERROR: " . mysqli_error($conn);
-    }
-
+    
 
 
 ?>
